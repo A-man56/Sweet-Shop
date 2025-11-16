b34a576f32a49733744ef0bcd602902d7adf0c8a
A-man56 <singhamanjeet87@gmail.com>
2025-11-16 10:49:12 +0530

    update  sweet.model

---
+++ models/Sweet.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const sweetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0
+  },
+  description: String,
+  image: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Sweet = mongoose.model('Sweet', sweetSchema);
+
+export default Sweet;
